3144ccf196cbe02fc8192126fc0fad522c4eeb8f
vishnuc45 <vishnu.c@technovia.co.in>
2026-01-22 16:14:15 +0530

    updated 2026

--- first/settings.py
+++ first/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cms',
+        'NAME': 'kmr',
         'USER': 'tis',
         'PASSWORD': 'tis',
         'PORT': '3306'
